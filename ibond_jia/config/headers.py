29f41dd7fc3b46c224554337dd0d6c8ec8dd6ce3
jiajinlong0301 <jiajinlong0301@hotmail.com>
2020-11-18 18:36:13 +0800

    优化获取cookies

--- ibond_jia/config/headers.py
+++ ibond_jia/config/headers.py
@@ -1,20 +1,39 @@
+from config.conf import *
+import requests
+
+def user_login_cookies():
+    """
+    获取登录用户cookie
+    :return: 返回用户cookie和csrfToken
+    """
+    url = server_ip() + "/bridgeApi/user/login" #登录接口
+    data={
+         'account': 'admin',
+         'password': 'ZAFWPv9tgy4/HEsujHm6JFzvL+7GW+QMVNN5S+dCpDHTF75SQ7rBlPKcqIiXbZiPHeHuJlYcNdpbKB+2llI7tK5lS4LJbfA0sEJfEw4JGuORPItoAwSaKyg23UTZ9DapuHSwEmvx6S99gCJhcFJhjre4b7KJwGxQPeg184MPcsI='
+        } #登录参数
+    r=requests.post(url=url,data=data) #请求登录
+    res=r.json()
+    csrfToken=res['data']['csrfToken']
+    cookies=r.headers['Set-Cookie']
+    return (cookies,csrfToken)
+
+cookie,csrtoken=user_login_cookies()
+
 def headers():
-    """
-    定义请求头
-    :return: 返回请求头
-    """
     headers = {
         'Accept': 'application/json, text/plain, */*',
         'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
         'Cache-Control': 'no-cache',
         'Connection': 'keep-alive',
+        'Content - Type': 'application / json;charset = UTF - 8',
+        'Cookie': cookie,
+        #'Host': '10.58.14.33:8080',
+        #'Origin': 'http://10.58.14.33:8080',
         'Pragma': 'no-cache',
+        'Referer': 'http://10.58.14.33:8080/model/drill',
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
-        'Host': '10.58.14.33:8080',
-        'Origin': 'http://10.58.14.33:8080',
-        'Referer': 'http://10.58.14.33:8080/login',
-        'X-Cf-Random': 'null',
+        'X-Cf-Random': csrtoken,
         'X-Requested-Width': 'XMLHttpRequest'
     }
     return headers
--- ibond_jia/test_case/test_update_params.py
+++ ibond_jia/test_case/test_update_params.py
@@ -1,20 +1,23 @@
-from config.get_login_cookies import *
+from config.headers import headers,server_ip
 import requests
 
-def update_params(header):
+def update_params():
+    """
+    更新算子参数
+    :param header: 传入请求头
+    :return:
+    """
     url = server_ip() + "/api/operator/update_params"
     data = {
         "operator_instance_id": 1027,
         "params": {
             "device": "cpu:0",
             "learning_rate": "0.001",
-            "model_interval": "1",
+            "model_interval": "3",
             "total_epoches": "7"}
     }
-    res = requests.post(url=url, json=data, headers=header)
-    print(res.text)
-cookies = user_login_cookies()
-header['Cookie'] = cookies
-header['Referer'] = 'http://10.58.14.33:8080/model/drill'
-header['X-Cf-Random'] = 'Xy8OVL/Lxm3ZJgvXEHYQ7NkMbYsDS3w6PKiu3lGVAQGRRPSRh/cL6RS17IfMtZc2nPHjjdfGImX5bY0ME7/PyeLL61StTMNIiLAVzfXualdU5n9EOt7JLMTLxapeqJU3Yp/S2DK8VsSEC1vO+G4uVSXJhGizmk6D3P7fwkYcf5U='
-update_params(header)
+    headers()['Referer'] = 'http://10.58.14.33:8080/login'
+    res = requests.post(url=url, json=data, headers=headers())
+    assert res.json()['success'] == True
+
+update_params()
